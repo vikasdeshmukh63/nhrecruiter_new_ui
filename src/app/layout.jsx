fa05d05ee03b950915586802788aed0773cef180
vikasdeshmukh63 <vikasdeshmukh63@gmail.com>
2024-10-18 15:24:09 +0530

    first commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,58 @@
+import 'src/global.css';
+
+// ----------------------------------------------------------------------
+
+import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
+
+import { CONFIG } from 'src/config-global';
+import { primary } from 'src/theme/core/palette';
+import { schemeConfig } from 'src/theme/scheme-config';
+import { ThemeProvider } from 'src/theme/theme-provider';
+
+import { ProgressBar } from 'src/components/progress-bar';
+import { MotionLazy } from 'src/components/animate/motion-lazy';
+import { SettingsDrawer, defaultSettings, SettingsProvider } from 'src/components/settings';
+
+import { AuthProvider } from 'src/auth/context/jwt';
+
+// ----------------------------------------------------------------------
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  themeColor: primary.main,
+};
+
+export const metadata = {
+  icons: [
+    {
+      rel: 'icon',
+      url: `${CONFIG.assetsDir}/favicon.ico`,
+    },
+  ],
+};
+
+export default async function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <InitColorSchemeScript
+          defaultMode={schemeConfig.defaultMode}
+          modeStorageKey={schemeConfig.modeStorageKey}
+        />
+
+        <AuthProvider>
+          <SettingsProvider settings={defaultSettings}>
+            <ThemeProvider>
+              <MotionLazy>
+                <ProgressBar />
+                <SettingsDrawer />
+                {children}
+              </MotionLazy>
+            </ThemeProvider>
+          </SettingsProvider>
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
