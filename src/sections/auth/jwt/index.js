385428795333d58b5dcdddf57ef67b5707946d1a
vikasdeshmukh63 <vikasdeshmukh63@gmail.com>
2024-10-22 16:40:21 +0530

    update

--- src/sections/auth/jwt/index.js
+++ src/sections/auth/jwt/index.js
@@ -1,7 +1,3 @@
-export * from './jwt-login-view';
-export * from './jwt-verify-view';
-
-export * from './jwt-register-view';
 export * from './jwt-reset-password-view';
 
 export * from './jwt-forgot-password-view';
